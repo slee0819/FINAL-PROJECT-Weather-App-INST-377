547a06d086d695f88b0822bdae385b21af191969
slee0819 <sanghwa4341@gmail.com>
2022-12-08 23:34:40 -0500

    Final LAB _ find the weather
    12/8 _ Demo working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cover = document.querySelector(".cover"),
-inputPart = cover.querySelector(".input-part"),
+inputPart = document.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
 inputField = inputPart.querySelector("input"),
-locationBtn = inputPart.querySelector("button");
+locationBtn = inputPart.querySelector("button"),
+weatherPart = cover.querySelector(".weather-part"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = cover.querySelector("header i");
 
 let api;
 
@@ -12,17 +15,22 @@ inputField.addEventListener("keyup", e =>{
     }
 });
 
-locationBtn.addEventListener("click", ()=>{
+locationBtn.addEventListener("click", () =>{
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(onSucces, onError);
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }else{
-        alert("Can not find the geolocation api")
+        alert("Please switch browser to support geolocation");
     }
 });
 
-function onSucces(position) {
+function requestApi(city){
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=764fbbd7a27293806d04e4e543acf0ba`;
+    fetchData();
+}
+
+function onSuccess(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=764fbbd7a27293806d04e4e543acf0ba`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=764fbbd7a27293806d04e4e543acf0ba`;
     fetchData();
 }
 
@@ -31,24 +39,50 @@ function onError(error){
     infoTxt.classList.add("error");
 }
 
-function requestApi(city){
-   api = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=$764fbbd7a27293806d04e4e543acf0ba';
-   fetchData();
-}
-
-function fetchData() {
-    infoTxt.innerText = " Search detail...";
+function fetchData(){
+    infoTxt.innerText = "More detail...";
     infoTxt.classList.add("pending");
-    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Try again";
+        infoTxt.classList.replace("pending", "error");
+    });
 }
 
-function weatherDetails(info) {
-    infoTxt.classList.replace("pending", "error");
+function weatherDetails(info){
     if(info.cod == "404"){
-        infoTxt.innerText = `${inputField.value} Not a Valid City Name`;
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = ` Please enter right city name !!!`;
     }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feels_like, humidity} = info.main;
+        if(id == 800){
+            wIcon.src = "image/clear.gif";
+        }else if(id >= 200 && id <= 232){
+            wIcon.src = "image/storm.gif";  
+        }else if(id >= 600 && id <= 622){
+            wIcon.src = "image/snow.gif";
+        }else if(id >= 701 && id <= 781){
+            wIcon.src = "image/haze.gif";
+        }else if(id >= 801 && id <= 804){
+            wIcon.src = "image/cloud.png";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            wIcon.src = "image/rain.gif";
+        }
+        
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb_2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
         infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
         cover.classList.add("active");
-        console.log(info);
     }
 }
+
+arrowBack.addEventListener("click", ()=>{
+    cover.classList.remove("active");
+});
